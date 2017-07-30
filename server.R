ed361d3b1854ad88db72cf87eba3d4d23d4c4fc0
ChaChaWoo <kdardia@verisk.com>
2017-07-30 19:57:49 -0400

    Update server.R

--- server.R
+++ server.R
@@ -4,67 +4,27 @@
 #
 # http://shiny.rstudio.com
 #
-
 library(shiny)
 
-# Read the file, select the data
-library(Hmisc)
-Ruff1 <- spss.get(file='RFFT reference sample.sav')
-Ruff1 <- Ruff1[Ruff1$Age >= 40,]   # For age <= 40, we do not have low and high education levels
-Ruff1 <- Ruff1[Ruff1$ISCED != 0,]  # ISCED 0 is not education except Kindergarten, and does
-                                   # no seem to fit in the picture (autodidacts?)
+data <- read.csv("hsb2.csv")
+colnames(data)[7] <- "Read"
+colnames(data)[9] <- "Math"
+colnames(data)[10] <- "Science"
 
-# Build the model
-fit <- lm(Unique ~ Age + ISCED, data=Ruff1)
-
-# Create data for the plotting grid
-plot.ages = seq(40, 75, length=9)
-plot.ISCEDs = seq(1,5,length=9)
-scores <- outer(X=plot.ages, Y=plot.ISCEDs, function(X,Y) predict(fit, data.frame(Age=X, ISCED=Y)))
-z.min = min(scores) # The have the z-value for the 'floor' of the plot
-
+## Define a server for the Shiny app
 shinyServer(function(input, output) {
-
-  predictedValue <- reactive({
-      predict(fit, newdata=data.frame(Age = input$Age, ISCED=as.numeric(input$Education)), type="response")
-  })      
+  
+  dataInput <- reactive({
+    subset(data, select=c(input$subject), subset=(substring(data$gender,1,4) == input$gender))
+  })
+  
+  ## Fill in the spot we created for a plot
+  output$plot1 <- renderPlot({
     
-  # PREDICTED VALUE     
-  output$predictedValue <- renderText({
-    paste(round(predictedValue()), "is the predicted number of unique designs from the model.")
+    ## Render a barplot
+    barplot(dataInput()[,input$subject],
+            main=paste(input$subject, "in", input$gender),
+            ylab="Subjecy", ylim=c(0,80),
+            xlab="Gender")
   })
-  
-  # PREDICTED INTERQUARTILE RANGE
-  output$interquartileRange <- renderText({
-    pred_interval <- predict(fit, newdata=data.frame(Age = input$Age, ISCED=as.numeric(input$Education)),
-                             interval="prediction", level=0.5)
-    paste('The model predicts that 50% of people with this age and education level will produce between',
-           round(pred_interval[2]), 'and', round(pred_interval[3]), 'figure designs in total on the test.')
-  })
-  
-  # PLOT
-  output$my.plot <- renderPlot({
-      pmat <- persp(x=plot.ages, y=plot.ISCEDs,z=scores, d=1.5, theta=25, phi=15, col="lightgreen",
-                    xlab="Age", ylab="Education level", zlab="Nr. of unique designs (predicted)",
-                    r=4, ticktype="detailed",
-                    main="Predicted value shown in a graphical representation of the Linear Model"
-                    )
-      fittedValue <- predictedValue()
-      points(trans3d(x=input$Age, y=as.numeric(input$Education), z=fittedValue, pmat=pmat),
-             col="darkgreen", pch=3, lwd=4, cex=1.6)
-      
-      lines.point1 <- as.data.frame(trans3d(x=input$Age, y=as.numeric(input$Education), z=fittedValue, pmat=pmat))
-      lines.point2 <- as.data.frame(trans3d(x=input$Age, y=1, z=fittedValue, pmat=pmat))
-      lines.point3 <- as.data.frame(trans3d(x=40, y=1, z=fittedValue, pmat=pmat))
-      horizontal.lines <- rbind(lines.point1, lines.point2, lines.point3)
-   
-      lines.point4 <- as.data.frame(trans3d(x=input$Age, y=1, z=z.min, pmat=pmat))
-      vertical.line <- rbind(lines.point2, lines.point4)
-       
-      lines(x=horizontal.lines, col="darkgreen", lwd=2)
-      lines(x=vertical.line, col="darkgreen", lwd=2)
-
-      
-  })
-
 })
